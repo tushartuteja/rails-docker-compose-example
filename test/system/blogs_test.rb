bdeac9bce16a270b9dbdad9a329497630c770ac0
Tushar Tuteja <ttuteja@gmail.com>
2018-12-15 13:34:54 +0530

    initial commit

---
+++ test/system/blogs_test.rb
@@ -0,0 +1,45 @@
+require "application_system_test_case"
+
+class BlogsTest < ApplicationSystemTestCase
+  setup do
+    @blog = blogs(:one)
+  end
+
+  test "visiting the index" do
+    visit blogs_url
+    assert_selector "h1", text: "Blogs"
+  end
+
+  test "creating a Blog" do
+    visit blogs_url
+    click_on "New Blog"
+
+    fill_in "Body", with: @blog.body
+    fill_in "Title", with: @blog.title
+    click_on "Create Blog"
+
+    assert_text "Blog was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Blog" do
+    visit blogs_url
+    click_on "Edit", match: :first
+
+    fill_in "Body", with: @blog.body
+    fill_in "Title", with: @blog.title
+    click_on "Update Blog"
+
+    assert_text "Blog was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Blog" do
+    visit blogs_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Blog was successfully destroyed"
+  end
+end
